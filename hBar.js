5d33de27db3a1acafb9f05fd9adc4a4d06332eca
Masa Fukui <fkmkfkmasum41@gmail.com>
2021-02-24 19:42:23 +0900

    Edit index.html

--- hBar.js
+++ hBar.js
@@ -1,8 +1,85 @@
+function drawTopChart(elem) {
+
+    ts = {
+        5:1,
+        12:3,
+        223:4
+    }
+
+    records = []
+
+    const vidLength = 360
+
+    for (let i=0; i<=vidLength; i++){
+        let record = {timestamp:i, comments:0}
+
+        if (i in ts) {
+            record.comments = ts[i] 
+        }
+        records.push(record)
+    }
+
+    
+    labels = []
+    comments = []
+
+
+    for (record of records) {
+        let date = new Date(null)
+        date.setSeconds(record.timestamp)
+        var result = date.toISOString().substr(11, 8);
+
+        labels.push(result)
+        comments.push(record.comments)
+        console.log(record)
+    }
+
+    
+    chart_data = []
+
+    for (let i=0; i<=labels.length; i++){
+        console.log('looping')
+        chart_data.push({
+            t: labels[i],
+            y: comments[i]
+        })
+    }
+
+    var myChart = new Chart(elem, {
+        type: 'line',
+        data: {
+            labels:labels,
+            datasets: [{
+                label: 'comments',
+                data: chart_data,
+                backgroundColor: [
+                    'rgba(255, 99, 132)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+            }
+        }
+    });
+
+    return myChart
+}
+
 function drawHBar(elem) {
     var myChart = new Chart(elem, {
         type: 'horizontalBar',
         data: {
-            labels: ['Red'],
+            labels: ['Comments'],
             datasets: [{
                 label: '# of Comments',
                 data: [12],
@@ -17,7 +94,7 @@ function drawHBar(elem) {
         },
         options: {
             scales: {
-                yAxes: [{
+                xAxes: [{
                     ticks: {
                         beginAtZero: true,
                     }
